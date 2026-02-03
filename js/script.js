cb029243d7882a68eb4d2df4d5e215fff37d69a9
ridhofebriann <ridhogans69@gmail.com>
2026-02-03 21:55:49 +0700

    commit

--- js/script.js
+++ js/script.js
@@ -15,19 +15,21 @@ form.addEventListener("submit", function (e) {
 
   const row = document.createElement("tr");
   row.innerHTML = `
-    <td>${task}</td>
-    <td>${dueDate}</td>
-    <td>Pending</td>
-    <td><button class="delete">Delete</button></td>
+    <td class="border border-gray-700 px-4 py-2">${task}</td>
+    <td class="border border-gray-700 px-4 py-2">${dueDate}</td>
+    <td class="border border-gray-700 px-4 py-2">Pending</td>
+    <td class="border border-gray-700 px-4 py-2">
+      <button class="delete px-3 py-1 bg-red-600 hover:bg-red-700 rounded text-white">Delete</button>
+    </td>
   `;
 
   todoList.appendChild(row);
-
   form.reset();
 });
 
 deleteAllBtn.addEventListener("click", function () {
-  todoList.innerHTML = '<tr><td colspan="4">No task found</td></tr>';
+  todoList.innerHTML =
+    '<tr><td colspan="4" class="px-4 py-2">No task found</td></tr>';
 });
 
 todoList.addEventListener("click", function (e) {
